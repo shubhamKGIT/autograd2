f6bdc1866378bd4900ad5147a7ec54db0579d738
Shubham Kesharwani <shubham.ksrwn@gmail.com>
2020-09-01 04:54:47 +0530

    commit or part02 covering broadcasting

--- autograd/tensor.py
+++ autograd/tensor.py
@@ -81,4 +81,52 @@ def tensor_sum(t: Tensor) -> Tensor:
     return Tensor(data,
                   requires_grad,
                   depends_on)
+
+def add(t1: Tensor, t2: Tensor) -> Tensor:
+    data = t1.data + t2.data
+    requires_grad = t1.requires_grad or t2.requires_grad
+    
+    depends_on: List[Dependency] = []
+
+    if t1.requires_grad:
+        def grad_fn1(grad: np.ndarray) -> np.ndarray:
+            # Idea: [1, 2, 3] + [2+e, 3, 4] = [3+e, 5, 7]
+            # Every change in one of the tensors is 
+            # reflected in the sum
+
+            # To handle broadcasting properly 
+            ndims_added = grad.ndim - t1.data.ndim
+            for _ in range(ndims_added):
+                grad = grad.sum(axis=0)
+
+            for i , dim in enumerate(t1.shape):
+                if dim ==1:
+                    grad = grad.sum(axis=i, keepdims=True)
+
+            return grad    # return whatever is the gradient with tensors
+        
+        depends_on.append(Dependency(t1, grad_fn1))
+
+    if t2.requires_grad:
+        def grad_fn2(grad: np.ndarray) -> np.ndarray:
+            
+            #### Handling broadcasting ####
+            # Sum across added dims
+            ndims_added = grad.ndim - t2.data.ndim
+            for _ in range(ndims_added):
+                grad = grad.sum(axis=0)
+
+            # Sum across broadcasted (but non-added dims)
+            for i , dim in enumerate(t2.shape):
+                if dim ==1:
+                    grad = grad.sum(axis=i, keepdims=True)
+
+            return grad 
+
+        depends_on.append(Dependency(t2, grad_fn2))
+
+    return Tensor(data,
+                  requires_grad,
+                  depends_on)
+
     
---
+++ tests/test_tensor_add.py
@@ -0,0 +1,40 @@
+import unittest
+import pytest
+
+from autograd.tensor import Tensor, add
+
+
+class TestTensorsum(unittest.TestCase):
+    def test_simple_add(self):
+        t1 = Tensor([1, 2, 3], requires_grad=True)
+        t2 = Tensor([4, 5, 6], requires_grad=True)
+
+        t3 = add(t1, t2)
+        t3.backward(Tensor([-1, -2, -3]))
+
+        assert t1.grad.data.tolist() == [-1, -2, -3]
+        assert t2.grad.data.tolist() == [-1, -2, -3]
+
+    def test_broadcast_add(self):
+        # Broadcasting?
+        # Add 1s to beginning of each shape when multiplying two matrices
+        # Default is t1*t2 when t1.shape, t2.shape is given 
+
+        # t1.shape ==(10,5), t2.shape==(5,) => t1+t2, t2 viewed as (1, 5)
+
+        # Second thing: If t2 has one dimension extra
+        # We can add t1 as one row to t2
+
+        t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
+        t2 = Tensor([7, 8, 9], requires_grad=True)
+
+        t3 = add(t1, t2)
+
+        t3.backward(Tensor([[1, 1, 1], [1, 1, 1]]))
+
+        assert t1.grad.data.tolist() == [[1, 1, 1],[1, 1, 1]]
+        assert t2.grad.data.tolist() == [2, 2, 2]
+
+    def test_broadcast_add2(self):
+        t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
+        t2 = Tensor([[7, 8, 9]], requires_grad=True)
