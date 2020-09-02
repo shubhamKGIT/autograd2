79ac4aa09d23e9a55dae69ba78aca48bc2a54dc2
Shubham Kesharwani <shubham.ksrwn@gmail.com>
2020-09-03 02:30:16 +0530

    Part 05

--- autograd/tensor.py
+++ autograd/tensor.py
@@ -30,15 +30,24 @@ class Tensor:
                  data: Arrayable,
                  requires_grad: bool = False,
                  depends_on: List[Dependency] = None) -> None:
-        self.data = ensure_array(data)
+        self._data = ensure_array(data)
         self.requires_grad = requires_grad
         self.depends_on = depends_on or []
-        self.shape = self.data.shape
+        self.shape = self._data.shape
         self.grad: Optional['Tensor'] = None
 
         if self.requires_grad:
             self.zero_grad()
-        
+
+    @property
+    def data(self) -> np.ndarray:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: np.ndarray) -> None:
+        self._data = new_data
+        # setting the data manually means we invalidate the gradient
+        self.grad = None
     
     def zero_grad(self) -> None:
         self.grad = Tensor(np.zeros_like(self.data))
@@ -62,9 +71,7 @@ class Tensor:
     def __iadd__(self, other) -> 'Tensor':
         """ when we do t+= other"""
         self.data = self.data + ensure_tensor(other).data
-        # invalidate the gradient 
-        self.grad = None
-        
+        # need not invalidate grad, will happen automatically
         return self
     
     def __isub__(self, other) -> 'Tensor':
@@ -89,6 +96,10 @@ class Tensor:
     def __rmul__(self, other) -> 'Tensor':
         return _mul(ensure_tensor(other), self)
 
+    def __matmul__(self, other) -> 'Tensor':
+        return _matmul(self, other)
+        
+
     def __neg__(self) -> 'Tensor':
         return _neg(self)
     
@@ -97,6 +108,10 @@ class Tensor:
 
     def __rsub__(self, other) -> 'Tensor':
         return _sub(ensure_tensor(other), self)
+
+    def __getitem__(self, idxs) -> 'Tensor':
+        return _slice(self, idxs)
+
 
     def backward(self, grad: 'Tensor' = None) -> None:
         assert self.requires_grad, "called backward on non-requires-grad-tensor"
@@ -257,3 +272,54 @@ def _neg(t: Tensor) -> Tensor:
 def _sub(t1: Tensor, t2: Tensor) -> Tensor:
     # return _add(t1, _neg(t2))
     return t1 + -t2
+
+def _matmul(t1: Tensor, t2: Tensor) -> Tensor:
+    """
+    If t1 is (n1, m1) and t2 is (m1, m2), then t1 @ t2 is (n1, m2)
+    so grad is (n1, m2)
+    if t3 = t1 @ t2 adn grad3 is gradient of some function wrt t3, 
+    then 
+    grad1 = grad @ t2.T
+    grad2 = t1.T @ grad
+    """
+    data = t1.data @ t2.data
+    requires_grad = t1.requires_grad or t2.requires_grad
+    
+    depends_on: List[Dependency] = []
+
+    if t1.requires_grad:
+        def grad_fn1(grad: np.ndarray) -> np.ndarray:
+            #grad = grad * t2.data
+            return grad @ t2.data.T
+
+        depends_on.append(Dependency(t1, grad_fn1))
+
+    if t2.requires_grad:
+        def grad_fn2(grad: np.ndarray) -> np.ndarray:
+            #grad = grad * t1.data
+            return t1.data.T @ grad
+            
+        depends_on.append(Dependency(t2, grad_fn2))
+
+    return Tensor(data,
+                  requires_grad,
+                  depends_on)
+    
+
+def _slice(t: Tensor, idxs) -> Tensor:
+    data = t.data[idxs]
+    requires_grad = t.requires_grad
+
+    if requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            bigger_grad = np.zeros_like(data)
+            bigger_grad[idxs] = grad 
+            return bigger_grad
+
+        depends_on = Dependency(t, grad_fn)
+
+    else:
+        depends_on =[]
+
+    return Tensor(data, requires_grad, depends_on)
+
